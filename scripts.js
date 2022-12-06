d968e02d4220becb6e6d075671731bf52ab0fa32
José Anderson <jose.andersonjr2@gmail.com>
2022-12-06 12:50:09 -0300

    Adição de comentários ao código

--- scripts.js
+++ scripts.js
@@ -1,31 +1,31 @@
-const setInputState = (el, state) => {
-    if (state === 'indeterminate') {
-      el.indeterminate = true
+const setInputState = (elemento, estado) => { // Define o estado de um elemento
+    if (estado === 'indeterminate') { // Se o estado for "indeterminado"
+      elemento.indeterminate = true // o elemento receber o valor como "indeterminado"
     } else {
-      el.indeterminate = false
-      el.checked = state 
+      elemento.indeterminate = false // 
+      elemento.checked = estado // Elemento recebo o estado que foi passado como parâmetro
     }
   }
   
-  const updateOwned = (el) => {
-    if (el.hasAttribute('data-children')) {
-      let state = el.checked
-      el.getAttribute('data-children').split(' ').forEach(id => {
-        let owned = document.getElementById(id)
-        setInputState(owned, state)
-        updateOwned(owned)
+  const atualizarControlado = (elemento) => { // Atualizar os atributos dos filhos
+    if (elemento.hasAttribute('data-children')) { //Se o elemento tiver o atributo data-children 
+      let estado = elemento.checked // variável "estado" recebe o valor atual dos elemento
+      elemento.getAttribute('data-children').split(' ').forEach(id => { //Separa os ids de cada filho e faz um laço para cada filho 
+        let controlado = document.getElementById(id) //Pega o elemento filho em questão 
+        setInputState(controlado, estado) // e define o estado dele para o mesmo estado do elemento pai
+        atualizarControlado(controlado)
       })
     }
   }
   
-  const updateOwner = (el) => {
-    if (el.hasAttribute('data-parent')) {
-      let owner = document.getElementById(el.getAttribute('data-parent'))
+  const updateControlador = (elemento) => { // Atualizar os atributos dos pais 
+    if (elemento.hasAttribute('data-parent')) { //Se o elemento tiver o atributo data-parent
+      let owner = document.getElementById(elemento.getAttribute('data-parent')) // Pega o elemento pelo id, o id desse elemento é o conteúdo dentro de "data-parent"
       let states = []
       let collectiveState
       owner.getAttribute('data-children').split(' ').every(id => {
-        let owned = document.getElementById(id)
-        let state = owned.indeterminate === true ? 'indeterminate' : owned.checked
+        let controlado = document.getElementById(id)
+        let state = controlado.indeterminate === true ? 'indeterminate' : controlado.checked
         if (states.length > 0 && states.indexOf(state) === -1) {
           collectiveState = 'indeterminate'
           return false
@@ -36,15 +36,15 @@ const setInputState = (el, state) => {
       })
       collectiveState = collectiveState || states[0]
       setInputState(owner, collectiveState)
-      updateOwner(owner)
+      updateControlador(owner)
     }
   }
   
   document.querySelectorAll('.nested-multiselect').forEach(multiselect => {
     multiselect.querySelectorAll('input[type="checkbox"][data-children], input[type="checkbox"][data-parent]').forEach(input => {
       input.addEventListener('change', event => {
-        updateOwned(event.currentTarget)
-        updateOwner(event.currentTarget)
+        atualizarControlado(event.currentTarget)
+        updateControlador(event.currentTarget)
       })
     })
   })
